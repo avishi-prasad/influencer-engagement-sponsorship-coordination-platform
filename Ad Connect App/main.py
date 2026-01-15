ed54c3d31c8748bfb5775474817860685bec3c39
Avishi Prasad <avishiprsd04@gmail.com>
2026-01-15 22:50:13 +0530

    Update main.py

--- Ad Connect App/main.py
+++ Ad Connect App/main.py
@@ -21,7 +21,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True            
     app.config['MAIL_USE_SSL'] = False           
     app.config['MAIL_USERNAME'] = 'avishiprsd04@gmail.com'  
-    app.config['MAIL_PASSWORD'] = 'qrby rukx bbaf piuv'
+    app.config['MAIL_PASSWORD'] = 'hidden'
     app.config['MAIL_DEFAULT_SENDER'] = 'avishiprsd04@gmail.com'
     app.config['MAIL_DEBUG'] = True
 
@@ -59,3 +59,4 @@ app=create_app()
 
 if __name__=="__main__":
     app.run(debug=True)
+
